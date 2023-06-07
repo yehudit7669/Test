aa34eb93d40af598e1fc6d9b0eb8c9dc6c57a711
wizerShrey <shrey@wizer.me>
2023-06-07 12:55:31 +0530

    Changes made to join wizer as a role page

--- src/components/pages/Auth/JoinWizerSignUpPage.tsx
+++ src/components/pages/Auth/JoinWizerSignUpPage.tsx
@@ -25,7 +25,7 @@ function JoinWizerSignUpPage() {
   const { t } = useTranslation();
   const dispatch = useAppDispatch();
   const user = useAppSelector((state: any) => state.auth.user);
-  const [, setToken] = useLocalStorage();
+  const [token, setToken] = useLocalStorage();
   const isAuthenticated = useUser();
 
   /* Routing, navigation and param dependencies */
@@ -52,7 +52,7 @@ function JoinWizerSignUpPage() {
   }, [navigate]);
 
   useEffect(() => {
-    dispatch(Actions.createAction(Actions.USER_LOGIN, { name: "karan" }));
+
   }, []);
 
   const renderLogInButton = () => (
@@ -134,7 +134,7 @@ function JoinWizerSignUpPage() {
             variant="contained"
             fullWidth
             color="secondary"
-            onClick={() => handleSignIn()}
+            onClick={() => {}}
           >
             {t("JoinWizerSignUp.signUp")}
           </Button>
@@ -167,47 +167,6 @@ function JoinWizerSignUpPage() {
       );
   };
 
-  const handleSignIn = () => {
-    let validInputs = true;
-    if (!email || !password) {
-      validInputs = false;
-      setError("Please fill all details.");
-    } else if (
-      !/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
-        email
-      )
-    ) {
-      validInputs = false;
-      setError("Please enter valid email address.");
-    } else {
-      validInputs = true;
-      setError("");
-    }
-    if (validInputs) {
-      //TODO : ADD api call code
-      dispatch(getUser("email", "password", true))
-        .then(async(response) => {
-          if (response.status === 200) {
-            const { token, isBirthDate } = response.data;
-            setToken(token);
-            // Navigate based on role and first sign in
-            if(!isBirthDate){
-              navigate(routes.AUTH + "/" + routes.BIRTH_DATE);
-            }
-          }
-        })
-        .catch((error) => {
-          setError(error.response.data.message);
-        });
-
-      //Temp solution
-      let jwt =
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiBEb2UiLCJyb2xlIjoic3R1ZGVudCIsImhhc1NpZ25lZEluQmVmb3JlIjp0cnVlfQ.1DhPob3HaXa22UEWn6Wn5aSBt8KuCwJdJa169b_J7tM";
-      setToken(jwt);
-      navigate(from, { replace: true });
-      console.log("Log in successful");
-    }
-  };
 
   return (
     <div className="SignIn">
