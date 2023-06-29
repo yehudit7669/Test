a7c82810f23fb14e4503a601a42509138a1a4e5b
Reactive-Karan <karanmishra224@gmail.com>
2023-06-29 12:43:14 +0530

    update husky linstagerc

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,11 +3,12 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import SignIn from '../components/pages/Auth/SignIn'
 import SignUpTabs from '../components/pages/Auth/SignUpTabs'
 import AuthLayout from '../components/layouts/AuthLayout'
-import { routes, userLayouts } from '../constants'
+import { routes, userLayouts, userRoles } from '../constants'
 import { connect as WSConnect } from '../socket'
 import SignUp from '../components/pages/Auth/SignUp'
 import renderRoutes from './routes/renderRoutes'
 import Worksheet from '../components/pages/Worksheet'
+import RequireAuth from './requireAuth/RequireAuth'
 
 function App() {
   useEffect(() => {
@@ -16,7 +17,6 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path={routes.WORKSHEET} element={<Worksheet />} />
         <Route element={<AuthLayout />}>
           {/* Index route - If user goes to '/' then navigate him to '/sign-in' */}
           <Route
@@ -36,6 +36,9 @@ function App() {
 
         {/* Protected routes for main layout */}
         {renderRoutes(userLayouts.IS_MAIN)}
+        <Route element={<RequireAuth allowedRole={userRoles.STUDENT} />}>
+          <Route path={routes.WORKSHEET} element={<Worksheet />} />
+        </Route>
       </Routes>
     </div>
   )
--- src/app/requireAuth/RequireAuth.tsx
+++ src/app/requireAuth/RequireAuth.tsx
@@ -12,7 +12,7 @@ const RequireAuth = ({ allowedRole }: Props) => {
 
   // Check if user and user role exist, and if the allowed roles include the user's role
   // const isAuthorized = user?.role === allowedRole
-  const isAuthorized = true;
+  const isAuthorized = user?.role === allowedRole
 
   return isAuthorized ? (
     <Outlet />
