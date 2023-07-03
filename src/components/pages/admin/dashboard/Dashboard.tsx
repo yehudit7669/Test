69927b1a3acacb0247518bfd6fa85bdffe1dc584
yehudit <105608961+yehudit7669@users.noreply.github.com>
2023-07-03 09:39:22 +0300

    fixCssBody

--- src/components/pages/admin/dashboard/Dashboard.tsx
+++ src/components/pages/admin/dashboard/Dashboard.tsx
@@ -7,19 +7,21 @@ import { useNavigate } from 'react-router'
 export default function Dashboard() {
   const navigate = useNavigate()
   return (
-    <div className="listSpaces">
-      <div
-        className="divButton"
-        onClick={() => navigate(`/admin/dashboard/new-customer`)}
-      >
-        <Button variant="contained" className="buttonNewDeal">
-          + Create new deal
-        </Button>
+    <div className="bodyPage">
+      <div className="listSpaces">
+        <div
+          className="divButton"
+          onClick={() => navigate(`/admin/dashboard/new-customer`)}
+        >
+          <Button variant="contained" className="buttonNewDeal">
+            + Create new deal
+          </Button>
+        </div>
+        <h1 className="quoteRequestTitle">Quote request</h1>
+        <QuoteRequestList />
+        <h1 className="customerListTitle">Customer list</h1>
+        <CustomerList />
       </div>
-      <h1 className="quoteRequestTitle">Quote request</h1>
-      <QuoteRequestList />
-      <h1 className="customerListTitle">Customer list</h1>
-      <CustomerList />
     </div>
   )
 }
