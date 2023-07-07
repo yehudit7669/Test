b14268408927d7ba78a25417ce5f7f7fb4495643
Reactive-Karan <karanmishra224@gmail.com>
2023-07-07 13:20:51 +0530

    image widget

--- src/components/pages/Worksheet/widget/common/image/ImageWidget.tsx
+++ src/components/pages/Worksheet/widget/common/image/ImageWidget.tsx
@@ -1,6 +1,25 @@
-const ImageWidget = (data: any) => {
-  console.log('Image widget here', data)
-  return <div>ImageWidget</div>
+import { Grid } from '@mui/material'
+import RenderQuestionHeader from '../renderQuestions/RenderQuestionHeader'
+import RichTextEditor from '../richTextEditor'
+
+const ImageWidget = ({ data }: any) => {
+  return (
+    <div className="Widget_Container">
+      <Grid container spacing={2}>
+        <RenderQuestionHeader title={data?.title} desc={data?.description} />
+        <Grid item xs={12} textAlign={'center'}>
+          <img
+            src={data?.image?.url}
+            height={data?.image?.height}
+            width={data?.image?.width}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <RichTextEditor />
+        </Grid>
+      </Grid>
+    </div>
+  )
 }
 
 export default ImageWidget
