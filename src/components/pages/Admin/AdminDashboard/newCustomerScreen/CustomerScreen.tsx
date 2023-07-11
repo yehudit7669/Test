6192d59ec71cfb011d3be56f43b90fdf43e9abd2
yocheved-fink <136968053+yocheved-fink@users.noreply.github.com>
2023-07-11 20:48:51 +0300

    fixed prettier

--- src/components/pages/Admin/AdminDashboard/newCustomerScreen/CustomerScreen.tsx
+++ src/components/pages/Admin/AdminDashboard/newCustomerScreen/CustomerScreen.tsx
@@ -28,7 +28,7 @@ export const CustomerScreen = () => {
   const [openSchoolLinkPopup, setOpenSchoolLinkPopup] = useState<boolean>(false)
   const [schoolLink, setSchoolLink] = useState<any>(null)
   const [emailsToInvitation, setEmailsToInvitation] = useState<Array<string>>(
-    []
+    [],
   )
   const navigate = useNavigate()
   const location = useLocation().pathname
@@ -40,7 +40,7 @@ export const CustomerScreen = () => {
     if (isEdit) {
       const renderCustomer = async () => {
         const res = await dispatch(
-          getCustomer(setError, setLoading, String(params.id))
+          getCustomer(setError, setLoading, String(params.id)),
         )
         if (!error) {
           setCustomer(res?.data)
@@ -58,14 +58,14 @@ export const CustomerScreen = () => {
           setError,
           setLoading,
           emailsToInvitation,
-          schoolId
-        )
+          schoolId,
+        ),
       )
       if (error) {
         alert('error on send the emails')
       }
     },
-    [dispatch, emailsToInvitation, error]
+    [dispatch, emailsToInvitation, error],
   )
 
   const onSubmit = useCallback(
@@ -83,7 +83,7 @@ export const CustomerScreen = () => {
       }
       if (!isEdit) {
         const res = await dispatch(
-          newCustomerAction(customerData, setError, setLoading)
+          newCustomerAction(customerData, setError, setLoading),
         )
         if (!error) {
           sendInvitation(res?.data?.schoolId)
@@ -97,7 +97,7 @@ export const CustomerScreen = () => {
         }
       }
     },
-    [dispatch, error, isEdit, customer, sendInvitation]
+    [dispatch, error, isEdit, customer, sendInvitation],
   )
 
   const renderInitialValues = () => {
