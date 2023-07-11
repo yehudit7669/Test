6645f7224dc4b8cf762454afea48ef08285f8ab6
yocheved-fink <136968053+yocheved-fink@users.noreply.github.com>
2023-07-11 20:47:34 +0300

    fixed eslint

--- src/components/pages/Admin/AdminDashboard/newCustomerScreen/CustomerScreen.tsx
+++ src/components/pages/Admin/AdminDashboard/newCustomerScreen/CustomerScreen.tsx
@@ -28,7 +28,7 @@ export const CustomerScreen = () => {
   const [openSchoolLinkPopup, setOpenSchoolLinkPopup] = useState<boolean>(false)
   const [schoolLink, setSchoolLink] = useState<any>(null)
   const [emailsToInvitation, setEmailsToInvitation] = useState<Array<string>>(
-    [],
+    []
   )
   const navigate = useNavigate()
   const location = useLocation().pathname
@@ -36,18 +36,19 @@ export const CustomerScreen = () => {
   const isEdit = location.includes('edit') ? true : false
   const params = useParams()
 
-  const renderCustomer = useCallback(async () => {
-    const res = await dispatch(
-      getCustomer(setError, setLoading, String(params.id)),
-    )
-    if (!error) {
-      setCustomer(res?.data)
-      console.log('customer', customer)
-    }
-  }, [dispatch, error, customer, params.id])
-
   useEffect(() => {
-    if (isEdit) renderCustomer()
+    if (isEdit) {
+      const renderCustomer = async () => {
+        const res = await dispatch(
+          getCustomer(setError, setLoading, String(params.id))
+        )
+        if (!error) {
+          setCustomer(res?.data)
+          console.log('customer', customer)
+        }
+      }
+      renderCustomer()
+    }
   }, [isEdit])
 
   const sendInvitation = useCallback(
@@ -57,14 +58,14 @@ export const CustomerScreen = () => {
           setError,
           setLoading,
           emailsToInvitation,
-          schoolId,
-        ),
+          schoolId
+        )
       )
       if (error) {
         alert('error on send the emails')
       }
     },
-    [dispatch, emailsToInvitation, error],
+    [dispatch, emailsToInvitation, error]
   )
 
   const onSubmit = useCallback(
@@ -82,7 +83,7 @@ export const CustomerScreen = () => {
       }
       if (!isEdit) {
         const res = await dispatch(
-          newCustomerAction(customerData, setError, setLoading),
+          newCustomerAction(customerData, setError, setLoading)
         )
         if (!error) {
           sendInvitation(res?.data?.schoolId)
@@ -96,7 +97,7 @@ export const CustomerScreen = () => {
         }
       }
     },
-    [dispatch, error, isEdit, customer, sendInvitation],
+    [dispatch, error, isEdit, customer, sendInvitation]
   )
 
   const renderInitialValues = () => {
